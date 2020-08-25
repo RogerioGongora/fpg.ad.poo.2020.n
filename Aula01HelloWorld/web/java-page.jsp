9b0ccaf7dcb208e57b4592be1a62770da7a9b882
rogerioalex2019PG <51092332+rogerioalex2019PG@users.noreply.github.com>
2020-08-25 18:50:45 -0300

    Projetos
    Projetos

---
+++ Aula01HelloWorld/web/java-page.jsp
@@ -0,0 +1,20 @@
+<%-- 
+    Document   : java-page
+    Created on : 20 de ago de 2020, 16:36:05
+    Author     : Rogerio
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Java Page - HelloWorld</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+        <h2>Java Page</h2>
+        <div><%= new java.util.Date() %></div>
+        <a href="index.html">Voltar para a página inicial</a>
+    </body>
+</html>
---
+++ Aula02Servelets/src/java/servlets/GreetingServlet.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Calendar;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Rogerio
+ */
+@WebServlet(name = "GreetingServlet", urlPatterns = {"/greeting.html"})
+public class GreetingServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Saudação - JavaServlets</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>JavaServlets</h1>");
+            out.println("<h2>Saudação</h2>");
+            out.println("<div><a href='index.html'>Voltar</a></div>");
+            int h = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+            if(h<6){
+            out.println("<h3>Vá dormir</h3>");
+            }else if(h<12){
+                out.println("<h3>Bom dia!</h3>");
+            }else if(h<18){
+                out.println("<h3>Boa tarde</h3>");
+            }else{
+                out.println("<h3>Boa noite</h3>");
+            }
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
